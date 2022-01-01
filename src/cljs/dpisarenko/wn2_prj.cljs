84c11b2e7c9b5937a26092f939725859beaba462
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-01-01 16:06:36 +0100

    Now we can display the working title.

--- src/cljs/dpisarenko/wn2_prj.cljs
+++ src/cljs/dpisarenko/wn2_prj.cljs
@@ -1,12 +1,14 @@
 (ns dpisarenko.wn2-prj
-  (:require [dpisarenko.process.v2.entry-point
-             :refer [writing-process-v2]])
-  (:require [dpisarenko.works.wn2.entry-point
-            :refer [wn2-data]])
+  (:require
+    [dpisarenko.process.v2.entry-point
+     :refer [writing-process-v2]]
+    [dpisarenko.works.wn2.entry-point
+     :refer [wn2-data]]
+    )
   )
 
-(def data
+(def wn2-project-data
   {
-    :process writing-process-v2
-    :work wn2-data
+   :process writing-process-v2
+   :work    wn2-data
    })
--- src/cljs/dpisarenko/works/wn2/entry_point.cljs
+++ src/cljs/dpisarenko/works/wn2/entry_point.cljs
@@ -1,6 +1,8 @@
 (ns dpisarenko.works.wn2.entry-point)
 
+; TODO: Output working title
+
 (def wn2-data
   {
-
+    :working-title "White Niggers 2"
    })
